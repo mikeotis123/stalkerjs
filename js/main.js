24bf0647a4d7f757c8c292c1f5b0210e97f87c1c
mikeotis123 <michael.otis2006@gmail.com>
2017-12-07 05:17:25 -0600

    A simple js app that asks the user for their name

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is you Last Name?");
+var age = prompt("How old are you?");
+var fullName = firstName + lastName;
+console.log("Nice to meet you " + firstName + " " + lastName);
+console.log("you are " + age + " years old " + fullName );
